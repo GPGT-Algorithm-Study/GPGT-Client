e81ba880ac8f6c072b9839eac1d1444929462697
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-08-28 17:02:34 +0900

    파비콘, 타이틀 수정, 컬러 수정

--- src/style/globalStyle.jsx
+++ src/style/globalStyle.jsx
@@ -133,14 +133,14 @@ const baseStyle = css`
   }
 
   :root {
-    --color-primary: rgb(18, 146, 250);
+    --color-primary: #3362c5;
     --color-background: rgb(240, 240, 240);
     --color-textgrey: rgb(112, 112, 112);
     --color-textlight: rgb(150, 150, 150);
     --color-error: #f24343;
     --color-unchecked: rgb(220, 220, 220);
     --color-checked: #81cc67;
-    --color-point: #546ffa;
+    --color-point: #3362c5;
     --color-teamsun: #fff1f1;
     --color-teammoon: #fff8e7;
     --color-toggle: #69b5f8;
