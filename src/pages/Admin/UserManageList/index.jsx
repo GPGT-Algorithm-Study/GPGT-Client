9ad6c755dc25eb5adad63fb94e60fd0a833b555a
SunghoonCho2 <emforhs03150@gmail.com>
2023-09-10 19:30:21 +0900

    유저 삭제 기능 추가

--- src/pages/Admin/UserManageList/index.jsx
+++ src/pages/Admin/UserManageList/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAllUsers, postNewUser } from 'api/user';
+import { delUser, getAllUsers, postNewUser } from 'api/user';
 import useFetch from 'hooks/useFetch';
 import { useState } from 'react';
 import {
@@ -74,14 +74,33 @@ function UserManageList() {
       password: '',
     });
   };
-  //onClickUserDelete = () => {};
+  const onClickUserDelete = (user) => {
+    const isAgree = confirm(
+      '<' +
+        user.notionId +
+        '> 유저를 정말정말정말정말정말진짜로!!!!!\n삭제하시겠습니까?????????',
+    );
+    if (!isAgree) return;
+    delUser(user)
+      .then((res) => {
+        if (res.data.code !== 200)
+          //에러 처리
+          console.log(res);
+        return;
+      })
+      .catch((e) => {
+        const { data } = e.response;
+        if (data && data.code == 400) toast.error(data.message);
+      });
+    alert('삭제되었습니다...');
+  };
   return (
     <div>
       <Title>유저 목록 관리</Title>
       <VerticalUserListWrapper>
         {users.map((user) => (
           <UserItem key={user.notionId}>
-            <Button>유저 삭제</Button>
+            <Button onClick={() => onClickUserDelete(user)}>유저 삭제</Button>
             {user.notionId} {user.emoji} : 경고 {user.warning}회. 포인트{' '}
             {user.point}.{' '}
             {user.isYesterdaySolved ? '어제 안 풀었음' : '어제 풀었음'}
