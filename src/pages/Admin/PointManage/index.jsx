7dc7add7afb931703a96b09513ca00d4b9239826
SunghoonCho2 <emforhs03150@gmail.com>
2023-09-12 20:51:53 +0900

    포인트 관리 모달창 radio버튼으로 부여/차감모드 추가

--- src/pages/Admin/PointManage/index.jsx
+++ src/pages/Admin/PointManage/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormWrapper, Title, UserItem, VerticalUserListWrapper } from './style';
 import useFetch from 'hooks/useFetch';
 import { getAllUsers } from 'api/user';
 
 function PointManage() {
+  const [isPlusMode, setIsPlusMode] = useState(true);
   const [users] = useFetch(getAllUsers, []);
   return (
     <div>
-      <Title>포인트 부여/차감</Title>
+      <Title>
+        포인트 {isPlusMode ? '부여' : '차감'}
+        <fieldset>
+          <legend>
+            <label>
+              <input
+                type="radio"
+                placeholder="부여"
+                onChange={() => {
+                  setIsPlusMode(true);
+                }}
+                checked={isPlusMode}
+              ></input>
+              부여
+            </label>
+            <label>
+              <input
+                type="radio"
+                placeholder="차감"
+                onChange={() => {
+                  setIsPlusMode(false);
+                }}
+                checked={!isPlusMode}
+              ></input>
+              차감
+            </label>
+          </legend>
+        </fieldset>
+      </Title>
       <VerticalUserListWrapper>
         {users.map((user) => (
           <UserItem id={user.notionId}>
