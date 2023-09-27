9675f414719e11915529d58bd6545ae0b0a492c6
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-27 10:17:34 +0900

    style: 안쓰는 스타일 정리

--- src/pages/Board/Detail/style.jsx
+++ src/pages/Board/Detail/style.jsx
@@ -43,39 +43,8 @@ export const CommentWrapper = styled.div`
   padding: 0 10px 0 10px;
 `;
 
-export const Container = styled.div`
-  width: 90%;
-  height: 80vh;
-  overflow: scroll;
-  padding: 15px 30px 0 20px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: white;
-  --saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.5);
-  box-shadow:
-    0 0 0 1px var(--saf-0),
-    0 4px 12px 0 rgba(0, 0, 0, 0.12);
-  border-radius: 6px;
-  user-select: none;
-  padding: 30px 40px 30px;
-  z-index: 1012;
-  position: relative;
-`;
-
 export const BackButton = styled(IoArrowBackSharp)`
   color: var(--color-textgrey);
   margin-bottom: 20px;
   cursor: pointer;
-`;
-
-export const CreateModal = styled.div`
-  width: 100%;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  top: 0;
-  right: 0;
-  z-index: 1022;
-  background-color: rgba(0, 0, 0, 0.8);
 `;
