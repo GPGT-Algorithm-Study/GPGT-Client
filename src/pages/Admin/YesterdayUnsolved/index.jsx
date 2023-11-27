0f100cad5e813089234a023785ac9088acb9c4ef
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-11-27 15:22:51 +0900

    refector: 어제 안푼 사람 중 차단된 사용자 제외

--- src/pages/Admin/YesterdayUnsolved/index.jsx
+++ src/pages/Admin/YesterdayUnsolved/index.jsx
@@ -5,12 +5,15 @@ import useScroll from 'hooks/useScroll';
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import useSWR from 'swr';
 import fetcher from 'utils/fetcher';
+import { USER_PREFIX_URL } from 'utils/constants';
 
 function YesterdayUnsolved() {
   const { data: users } = useSWR(
     `/api/v1/stat/user/yesterday-unsolved-users`,
     fetcher,
   );
+
+  const { data: allUsers } = useSWR(`${USER_PREFIX_URL}/info/all`, fetcher);
 
   const [
     leftArrowHovering,
@@ -25,31 +28,40 @@ function YesterdayUnsolved() {
     window.open(url, '_blank');
   }, []);
 
-  if (!users) return null;
+  if (!users || !allUsers) return null;
 
   return (
     <Card>
       <Title>어제 안 푼 사람들</Title>
       <Content>
         <UserWrapper ref={horizontalScrollRef}>
-          {users.map((user) => (
-            <User key={user.notionId}>
-              <div>
-                {user.notionId} {user.emoji}
-              </div>
-              <CommonProfileImage
-                width="50"
-                height="50"
-                src={
-                  user.profileImg != 'null'
-                    ? user.profileImg
-                    : 'https://static.solved.ac/misc/360x360/default_profile.png'
-                }
-                onClick={(e) => onClickProfileImg(e, user.bojHandle)}
-                style={{ cursor: 'pointer' }}
-              ></CommonProfileImage>
-            </User>
-          ))}
+          {users.map((user) => {
+            if (
+              allUsers.some(
+                (item) =>
+                  item.bojHandle === user.bojHandle && item.warning === 4,
+              )
+            )
+              return null;
+            return (
+              <User key={user.notionId}>
+                <div>
+                  {user.notionId} {user.emoji}
+                </div>
+                <CommonProfileImage
+                  width="50"
+                  height="50"
+                  src={
+                    user.profileImg != 'null'
+                      ? user.profileImg
+                      : 'https://static.solved.ac/misc/360x360/default_profile.png'
+                  }
+                  onClick={(e) => onClickProfileImg(e, user.bojHandle)}
+                  style={{ cursor: 'pointer' }}
+                ></CommonProfileImage>
+              </User>
+            );
+          })}
         </UserWrapper>
         <ScrollButton
           onClick={() => {
