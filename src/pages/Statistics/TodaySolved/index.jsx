cdaae4519420d5a2a30227c12bb6d8e77e1e2062
Huiyeong <53117014+klloo@users.noreply.github.com>
2024-02-05 00:01:19 +0900

    feat: 통계 스켈레톤 ui 추가

--- src/pages/Statistics/TodaySolved/index.jsx
+++ src/pages/Statistics/TodaySolved/index.jsx
@@ -4,6 +4,7 @@ import SolvedIcon from 'components/SolvedIcon';
 import useSWR from 'swr';
 import { STAT_PREFIX_URL } from 'utils/constants';
 import fetcher from 'utils/fetcher';
+import Skeleton from 'react-loading-skeleton';
 
 function TodaySolved() {
   const { data: users } = useSWR(
@@ -11,23 +12,26 @@ function TodaySolved() {
     fetcher,
   );
 
-  if (!users) {
-    return null;
-  }
-
   return (
     <Card>
       <Title>오늘의 문제 해결 현황</Title>
       <Content>
         <UserWrapper>
-          {users.map((user) => (
-            <User key={user.notionId}>
-              <SolvedIcon solved={user.isTodaySolved} />
-              <div>
-                {user.notionId} {user.emoji}
-              </div>
-            </User>
-          ))}
+          {users
+            ? users.map((user) => (
+                <User key={user.notionId}>
+                  <SolvedIcon solved={user.isTodaySolved} />
+                  <div>
+                    {user.notionId} {user.emoji}
+                  </div>
+                </User>
+              ))
+            : new Array(12).fill(0).map((_, i) => (
+                <User key={i}>
+                  <Skeleton width={40} height={40} circle />
+                  <Skeleton width={70} />
+                </User>
+              ))}
         </UserWrapper>
       </Content>
     </Card>
--- src/pages/Statistics/WarningGraph/index.jsx
+++ src/pages/Statistics/WarningGraph/index.jsx
@@ -5,6 +5,7 @@ import useSWR from 'swr';
 import { USER_PREFIX_URL } from 'utils/constants';
 import fetcher from 'utils/fetcher';
 import WarningIcon from 'components/WarningIcon';
+import Skeleton from 'react-loading-skeleton';
 /**
  * 사용자별 경고 현황 그래프
  */
@@ -18,14 +19,21 @@ function WarningGraph() {
       <Title>경고 현황</Title>
       <Content>
         <UserWrapper>
-          {users.map((user) => (
-            <User key={user.notionId}>
-              <WarningIcon warning={user.warning} />
-              <div>
-                {user.notionId} {user.emoji}
-              </div>
-            </User>
-          ))}
+          {users
+            ? users.map((user) => (
+                <User key={user.notionId}>
+                  <WarningIcon warning={user.warning} />
+                  <div>
+                    {user.notionId} {user.emoji}
+                  </div>
+                </User>
+              ))
+            : new Array(12).fill(0).map((_, i) => (
+                <User key={i}>
+                  <Skeleton width={40} height={40} circle />
+                  <Skeleton width={70} />
+                </User>
+              ))}
         </UserWrapper>
       </Content>
     </Card>
