14abb509b0356982714704ca0e5869df04152401
SunghoonCho2 <emforhs03150@gmail.com>
2024-03-31 17:07:21 +0900

    [update] : 이벤트 핸들러 작성, 스켈레톤 로딩 화면 적용

--- src/pages/Complaint/ComplaintInputForm/index.jsx
+++ src/pages/Complaint/ComplaintInputForm/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Author, Card, ContentArea } from './style';
 import { USER_PREFIX_URL } from 'utils/constants';
 import useSWR from 'swr';
@@ -10,38 +10,74 @@ import {
   Form,
   FormItem,
 } from 'pages/Board/Write/style';
+import Skeleton from 'react-loading-skeleton';
+import { getAllComplaint } from 'api/complaint';
 
 function ComplaintInputForm() {
   const { data: loginUser } = useSWR(
     `${USER_PREFIX_URL}/auth/parse/boj`,
     fetcher,
   );
-  const { data: userInfo, mutate: mutaUserInfo } = useSWR(
-    `${USER_PREFIX_URL}/info?bojHandle=${loginUser.claim}`,
+  const { data: userInfo, mutate: mutateUserInfo } = useSWR(
+    `${USER_PREFIX_URL}/info?bojHandle=${loginUser?.claim}`,
     fetcher,
   );
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   /* TODO : API 연결 */
-  return (
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = (e) => {
+    console.log(title);
+    console.log(content);
+  };
+
+  return userInfo ? (
     <Container>
       <Form>
         <FormItem>
           <div>
-            <input id="titleInput" placeholder="제목"></input>
+            <input
+              id="titleInput"
+              placeholder="제목"
+              onChange={onChangeTitle}
+            ></input>
           </div>
         </FormItem>
         <FormItem>
-          <p>작성자 : {userInfo.notionId}</p>
+          <p>작성자 : {userInfo ? userInfo.notionId : '누구세요?'}</p>
         </FormItem>
         <FormItem>
           <ContentArea
             rows="10"
             id="content"
             placeholder="민원 내용 작성.."
+            onChange={onChangeContent}
           ></ContentArea>
         </FormItem>
-        <ButtonWrapper>
-          <Button>민원 제출</Button>
-        </ButtonWrapper>
+        <FormItem>
+          <ButtonWrapper>
+            <Button onClick={onSubmit}>민원 제출</Button>
+          </ButtonWrapper>
+        </FormItem>
+      </Form>
+    </Container>
+  ) : (
+    <Container>
+      <Form>
+        <FormItem>
+          <Skeleton width="80%" />
+        </FormItem>
+        <FormItem>
+          <Skeleton width="80%" />
+        </FormItem>
+        <FormItem>
+          <Skeleton width="80%" height={200} />
+        </FormItem>
       </Form>
     </Container>
   );
