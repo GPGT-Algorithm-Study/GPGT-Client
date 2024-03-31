92f95a0147956744584a7a70e9db4b1951837925
SunghoonCho2 <emforhs03150@gmail.com>
2024-03-31 20:53:14 +0900

    [UPDATE] : 민원 등록 기능 API 연결

--- src/pages/Complaint/ComplaintInputForm/index.jsx
+++ src/pages/Complaint/ComplaintInputForm/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Author, Card, ContentArea } from './style';
 import { USER_PREFIX_URL } from 'utils/constants';
 import useSWR from 'swr';
@@ -6,12 +6,20 @@ import fetcher from 'utils/fetcher';
 import {
   Button,
   ButtonWrapper,
+  Category,
+  CategoryWrapper,
   Container,
   Form,
   FormItem,
 } from 'pages/Board/Write/style';
 import Skeleton from 'react-loading-skeleton';
-import { getAllComplaint } from 'api/complaint';
+import {
+  createComplaint,
+  getAllComplaint,
+  getMyComplaint,
+} from 'api/complaint';
+import { isEmpty } from 'lodash';
+import { toast } from 'react-toastify';
 
 function ComplaintInputForm() {
   const { data: loginUser } = useSWR(
@@ -22,34 +30,66 @@ function ComplaintInputForm() {
     `${USER_PREFIX_URL}/info?bojHandle=${loginUser?.claim}`,
     fetcher,
   );
-  const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [complaintType, setComplaintType] = useState('');
+  const categories = [
+    { name: 'NEW_FUNCTION', description: '신규 기능 건의' },
+    { name: 'BUG', description: '버그 제보' },
+    { name: 'PROBLEM', description: '문제점' },
+    { name: 'ETC', description: '기타' },
+  ];
   /* TODO : API 연결 */
-  const onChangeTitle = (e) => {
-    setTitle(e.target.value);
-  };
-  const onChangeContent = (e) => {
+  const onChangeContent = useCallback((e) => {
     setContent(e.target.value);
-  };
-  const onSubmit = (e) => {
-    console.log(title);
-    console.log(content);
-  };
+  }, []);
+  const validate = useCallback(() => {
+    if (isEmpty(content.trim())) {
+      toast.error('내용을 입력해주세요.');
+      return false;
+    }
+    if (!categories.find((category) => category.name === complaintType)) {
+      toast.error('민원 유형을 선택해주세요.');
+      return false;
+    }
+  });
+  const onSubmit = useCallback((e) => {
+    if (validate() === false) return;
+    const newComplaint = {
+      requester: loginUser.claim,
+      content: content,
+      complaintType: complaintType,
+    };
+    createComplaint(newComplaint)
+      .then((res) => {
+        toast.success('민원을 등록했습니다.');
+      })
+      .catch((err) => {
+        toast.error(`민원 등록에 실패했습니다 : ${err.message}`);
+      });
+  });
 
   return userInfo ? (
     <Container>
       <Form>
         <FormItem>
-          <div>
-            <input
-              id="titleInput"
-              placeholder="제목"
-              onChange={onChangeTitle}
-            ></input>
-          </div>
+          <p>작성자 : {userInfo ? userInfo.notionId : '누구세요?'}</p>
         </FormItem>
         <FormItem>
-          <p>작성자 : {userInfo ? userInfo.notionId : '누구세요?'}</p>
+          <CategoryWrapper>
+            {categories.map((category) => {
+              return (
+                <Category
+                  key={category.name}
+                  selected={complaintType === category.name}
+                  onClick={() => {
+                    setComplaintType(category.name);
+                  }}
+                >
+                  {category.description}
+                </Category>
+              );
+            })}
+          </CategoryWrapper>
         </FormItem>
         <FormItem>
           <ContentArea
